4345a2c9eda292a15fd81ebaf7945e22b86383ce
moonshi <shoonmayedu@gmail.com>
2025-01-25 11:36:12 +0900

    feat: component, state, props learned'

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
-      </header>
-    </div>
-  );
+/*import React from "react";
+class Item extends React.Component {
+  render() {
+    return (<li>
+      {this.props.name},
+      ${this.props.price}
+    </li>)
+  }
 }
 
-export default App;
+class AddItem extends React.Component {
+
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  
+  add = () => {
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+    this.props.add(name,price)
+  }
+
+  render (){
+    return (
+      <div>
+        Item : <input type="text" ref={this.nameRef} style= {{marginRight:20}}/>
+        Price : <input type = "text" ref={this.priceRef} />
+        <button onClick={this.add} style= {{marginLeft:20}}>Add </button>
+      </div>
+    )
+  }
+}
+class App extends React.Component {
+  state = {
+    items: []
+  }
+
+  add= (name,price) => {
+    let id = this.state.items.length + 1;
+
+    this.setState({
+      items: [...this.state.items, { id, name, price }]
+    })
+  }
+
+  render() {
+    return (
+      <div >
+        <h3>Item Registration</h3>
+        <ul>
+          {
+            this.state.items.map(x => {
+              return (
+                <Item key={x.id} name={x.name} price={x.price} />
+              )
+            })
+          }
+        </ul>
+        <AddItem add = {this.add} />
+      </div>
+    )
+  }
+}
+
+export default App;*/
+import React from "react";
+class Title extends React.Component {
+  render(){
+    return (<h1>{this.props.name}</h1>)
+  }
+}
+
+class Header extends React.Component {
+  render (){
+    return (
+      <Title name ={this.props.name}/>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Header name = "App Title"/>
+    )
+  }
+}
+
+export default App
